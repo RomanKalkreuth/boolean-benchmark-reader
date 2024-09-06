87d6bbb72b1dd22ce70534979a55cafad2a5f99f
Roman Kalkreuth <roman.kalkreuth@rwth-aachen.de>
2024-09-06 13:55:20 +0200

    comments added

--- src/benchmark_evaluator.py
+++ src/benchmark_evaluator.py
@@ -11,7 +11,10 @@ class BenchmarkEvaluator:
     by calculating the the hamming distance. """
 
     def evaluate(self, x, y, compressed=False, chunk_size = None):
-        """ Triggers the evaluation with the Hamming distance """
+        """
+        Triggers the evaluation with the Hamming distance. DIstiguishes between compressed and uncompressed
+        output pairs that are passed to the function.
+        """
         if len(x) != len(y):
             raise ValueError("Dimensions do not match.")
 
@@ -21,13 +24,22 @@ class BenchmarkEvaluator:
             return self.hamming_distance(x, y, compressed=compressed, chunk_size=chunk_size)
 
     def hamming_distance(self, x, y, compressed=False, chunk_size = None):
-        """ Calculates the hamming distance which is defined as
-        the number of different bits between x and y"""
+        """
+        Calculates the hamming distance which is defined as
+        the number of different bits between x and y.
+
+        :param x:
+        :param y:
+        :compressed:
+        :chunk_size:
+        """
         dist = 0
         if compressed:
             for xi, yi in zip(x,y):
+                # Bitwise XOR the chunks to identify dissimilar bits
                 cmp = xi ^ yi
                 for i in range(chunk_size):
+                    # Sum up the number of 1s then
                     dist += cmp & 1
                     cmp = cmp >> 1
         else:
