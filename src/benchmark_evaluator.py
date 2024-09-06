719971e238d31b2054386a6ff1942124e7ad58cc
Roman Kalkreuth <roman.kalkreuth@rwth-aachen.de>
2024-09-06 14:07:38 +0200

    python documentation added

--- src/benchmark_evaluator.py
+++ src/benchmark_evaluator.py
@@ -10,10 +10,10 @@ class BenchmarkEvaluator:
     """ Evaluator class that evaluates compressed and uncompressed output pairs
     by calculating the the hamming distance. """
 
-    def evaluate(self, x, y, compressed=False, chunk_size = None):
+    def evaluate(self, x: list, y: list, compressed:bool = False, bit_length:int = None) -> int :
         """
-        Triggers the evaluation with the Hamming distance. DIstiguishes between compressed and uncompressed
-        output pairs that are passed to the function.
+        Triggers the evaluation with the Hamming distance. Distinguishes between compressed and uncompressed
+        output pairs of a candidate program/model and the real output values of a truth table that are passed to the function.
         """
         if len(x) != len(y):
             raise ValueError("Dimensions do not match.")
@@ -21,24 +21,24 @@ class BenchmarkEvaluator:
         if not compressed:
             return self.hamming_distance(x, y)
         else:
-            return self.hamming_distance(x, y, compressed=compressed, chunk_size=chunk_size)
+            return self.hamming_distance(x, y, compressed=compressed, bit_length=bit_length)
 
-    def hamming_distance(self, x, y, compressed=False, chunk_size = None):
+    def hamming_distance(self, x: list, y: list, compressed:bool = False, bit_length:int = None) -> int :
         """
         Calculates the hamming distance which is defined as
         the number of different bits between x and y.
 
-        :param x:
-        :param y:
-        :compressed:
-        :chunk_size:
+        :param x: list with output values from the candidate model or program
+        :param y: list with real output values 
+        :compressed: status of truthtable (compressed or uncompressed)
+        :chunk_size: number of bits for each chunk of an uncompressed truth table
         """
         dist = 0
         if compressed:
             for xi, yi in zip(x,y):
                 # Bitwise XOR the chunks to identify dissimilar bits
                 cmp = xi ^ yi
-                for i in range(chunk_size):
+                for i in range(bit_length):
                     # Sum up the number of 1s then
                     dist += cmp & 1
                     cmp = cmp >> 1
